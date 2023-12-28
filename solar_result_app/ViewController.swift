0a76a087528a25ced2bb8b29ab3dfd474c624ee6
mostro610 <moritz@strobel-design.de>
2023-12-28 18:13:39 +0100

    hammer

--- solar_result_app/ViewController.swift
+++ solar_result_app/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var new = 30;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
