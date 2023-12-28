f3ca7462f632ecb5c889d88d858da536a4988cc2
mostro610 <moritz@strobel-design.de>
2023-12-28 18:05:07 +0100

    Initial Commit

---
+++ solar_result_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  solar_result_app
+//
+//  Created by moritz strobel on 28.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ solar_result_appUITests/solar_result_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  solar_result_appUITestsLaunchTests.swift
+//  solar_result_appUITests
+//
+//  Created by moritz strobel on 28.12.23.
+//
+
+import XCTest
+
+final class solar_result_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
